1d31c7c869db25ca7dc2ecf7ecd6eb89d908da8c
jankotanime <jan.gasztold@gmail.com>
2024-10-31 16:36:06 +0100

    Added touchable image

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Alert, StyleSheet, Button } from 'react-native';
 import axios from 'axios';
 
 export default function App() {
   const [message, setMessage] = useState('');
   const [x, setX] = useState(3);
+  const settingsButton = () => {
+    Alert.alert('ustawienia');
+  }
   useEffect(() => {
     const fetchMessage = () => {
       // Twój lokalny adres IP oraz port backendu
@@ -25,9 +28,17 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.main_container}>
-        <Text style={styles.text_main}>{message ? message : 'Ładowanie...'}</Text>
-        <Button title="button" onPress={() => {setX(x + 1);}} />
+      <View style={styles.mask}>
+        <TouchableOpacity style={styles.button} onPress={settingsButton}>
+          <Image
+            source={require('./images/test.jpg')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <View style={styles.main_container}>
+          <Text style={styles.text_main}>{message ? message : 'Ładowanie...'}</Text>
+          <Button title="button" onPress={() => {setX(x + 1);}} />
+        </View>
       </View>
     </View>
   );
@@ -41,12 +52,27 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
+  mask: {
+    justifyContent: 'space-between',
+    height: '90%',
+    width: '95%',
+    alignItems: 'center',
+  },
+  button: {
+    height: 40,
+    width: '100%',
+  },
+  image: {
+    height: 40,
+    width: 40,
+    alignSelf: 'flex-end'
+  },
   main_container: {
     backgroundColor: 'rgb(250, 230, 210)',
     alignItems: 'center', 
     justifyContent: 'center',
-    width: '90%',
-    height: '80%',
+    width: '100%',
+    height: '92.5%',
       // Cień dla iOS
       shadowColor: '#000',
       shadowOffset: {
