2c69a7e143bda2b11507cd4440574de0d314b414
jankotanime <jan.gasztold@gmail.com>
2024-10-31 16:06:00 +0100

    working api, arguments frontend - backend

--- App.js
+++ App.js
@@ -1,28 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import axios from 'axios';
 
 export default function App() {
   const [message, setMessage] = useState('');
-
+  const [x, setX] = useState(3);
   useEffect(() => {
-    // Twój lokalny adres IP oraz port backendu
-    const serverUrl = 'http://192.168.0.13:3000/api/message'; // Wstaw swój adres IP
+    const fetchMessage = () => {
+      // Twój lokalny adres IP oraz port backendu
+      const serverUrl = `http://192.168.1.117:3000/api/message?x=${x}`; // Wstaw swój adres IP
+      // Wykonanie zapytania GET
+      axios.get(serverUrl)
+        .then(response => {
+          setMessage(response.data.message);
+        })
+        .catch(error => {
+          console.error('Błąd połączenia z serwerem:', error);
+          setMessage('Błąd połączenia z serwerem');
+        });
+    };
 
-    // Wykonanie zapytania GET
-    axios.get(serverUrl)
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('Błąd połączenia z serwerem:', error);
-        setMessage('Błąd połączenia z serwerem');
-      });
-  }, []);
+    fetchMessage();
+  }, [x]);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{message ? message : 'Ładowanie...'}</Text>
+    <View style={styles.screen}>
+      <View style={styles.main_container}>
+        <Text style={styles.text_main}>{message ? message : 'Ładowanie...'}</Text>
+        <Button title="button" onPress={() => {setX(x + 1);}} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: 'rgb(250, 230, 210)',
+    alignItems: 'center', 
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  main_container: {
+    backgroundColor: 'rgb(250, 230, 210)',
+    alignItems: 'center', 
+    justifyContent: 'center',
+    width: '90%',
+    height: '80%',
+      // Cień dla iOS
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 5,
+      },
+      shadowOpacity: 0.3,
+      shadowRadius: 6,
+      // Cień dla Androida
+      elevation: 10,
+  },
+  main: {  
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  text_main: {
+    color: 'rgb(30, 30, 30)',
+    fontSize: '25%',
+  }
+});
--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 // Struktura odpowiedzi JSON
@@ -12,23 +13,28 @@ type Message struct {
 	Message string `json:"message"`
 }
 
-func suma(lista ...int) int {
-	sum := 0
-	for _, j := range lista {
-		sum += j
-	}
-	return sum
+func suma(x int) int {
+	wynik := x * x
+	return wynik
 }
 
 // Handler dla endpointu "/api/message"
 func messageHandler(w http.ResponseWriter, r *http.Request) {
-	string := fmt.Sprintf("%d", suma(1, 2, 4, 6))
-	response := Message{string}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	xStr := r.URL.Query().Get("x")
+	x, err := strconv.Atoi(xStr)
+	if err == nil {
+		string := fmt.Sprintf("%d", suma(x))
+		response := Message{string}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	} else {
+		response := Message{"błąd!"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
 }
 
 func main() {
 	http.HandleFunc("/api/message", messageHandler)
-	http.ListenAndServe(":3000", nil) // Serwer uruchomi się na porcie 3000
+	http.ListenAndServe(":3000", nil)
 }
