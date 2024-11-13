41efc781e257439d27ae9a5e05e22be28bc30237
jankotanime <jan.gasztold@gmail.com>
2024-11-13 16:55:17 +0100

    added date and time picker

--- frontend/NewTask.js
+++ frontend/NewTask.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Button, TextInput, Alert, TouchableOpacity, Image, Animated, View, Text, StyleSheet, Dimensions, PanResponder, ScrollView } from 'react-native';
+import { Button, TextInput, Alert, TouchableOpacity, Image, Animated, View, Text, StyleSheet, Dimensions, Platform, PanResponder, ScrollView } from 'react-native';
 import axios from 'axios';
 import ip from './variables'
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -9,6 +9,20 @@ let setOpenedExternal;
 export default function NewTask({fetchMessage}) {
     const [isOpened, setOpened] = useState(false);
     const [inputText, setInputText] = useState('');
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('date'); // Tryb: 'date' lub 'time'
+    const [show, setShow] = useState(false); // Kontrola widoczności pickera
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios'); // Ukrywa picker po wyborze, jeśli to nie iOS
+        setDate(currentDate); // Ustawia wybraną datę lub godzinę
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true); // Pokazuje picker
+        setMode(currentMode); // Ustawia tryb ('date' lub 'time')
+    };
     setOpenedExternal = setOpened;
     if (isOpened) {
         return (
@@ -26,6 +40,20 @@ export default function NewTask({fetchMessage}) {
                     value={inputText}
                     onChangeText={text => setInputText(text)}
                 />
+                <Button title="Select Date" onPress={() => showMode('date')} />
+                <Button title="Select Time" onPress={() => showMode('time')} style={{ marginTop: 10 }} />
+                <Text style={{ marginTop: 20 }}>Selected Date: {date.toLocaleDateString()}</Text>
+                <Text style={{ marginTop: 10 }}>Selected Time: {date.toLocaleTimeString()}</Text>
+
+                {show && (
+                    <DateTimePicker
+                    value={date}
+                    mode={mode} // Tryb: 'date' lub 'time'
+                    is24Hour={true} // Dla 24-godzinnego formatu
+                    display="default" // Wygląd pickera
+                    onChange={onChange} // Obsługa zmiany
+                    />
+                )}
             </View>
         );
     }
